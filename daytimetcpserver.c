8150b0508648c5249bad1ab6c36e893e566ee898
marong <425862061@qq.com>
2017-04-25 01:44:41 +0800

    :q

--- daytimetcpserver.c
+++ daytimetcpserver.c
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
 		err_quit(0, "socket error");
 	}
 
-	struct sockaddr_in servaddr;
+	struct sockaddr_in servaddr, cliaddr;
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -56,21 +56,26 @@ int main(int argc, char **argv)
 	int connfd = 0;
 	time_t ticks;
 	char buff[MAX_LINE];
-	int len = 0;
+	socklen_t len;
 	for( ; ; )
 	{
-		connfd = accept(listenfd, NULL, NULL);
+		// connfd = accept(listenfd, NULL, NULL);  // 不关心客户端地址
+		len = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &len);
 		if(connfd < 0)
 		{
 			err_quit(1, "socket accept error");
 		}
 
+		printf("connection from %s, port %u\n",
+			inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+			ntohs(cliaddr.sin_port));
+
 		ticks = time(NULL);
 		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
 		printf("%s", buff);
 
-		len = strlen(buff);
-		if(write(connfd, buff, len) != len)
+		if(write(connfd, buff, len) != strlen(buff))
 		{
 			err_quit(1, "write error");
 		}
